bee96dff3a5002784840b86cf9558bb0e1671be1
Genevieve Blais <gblais@vfi.io>
2024-01-30 10:30:48 -0500

    commit etape 9

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,7 +1,9 @@
-from api.app import create_app
+from packages.ml_api.api.app import create_app
+from packages.ml_api.api.config import DevelopmentConfig
 
+application = create_app(
+    config_object=DevelopmentConfig)
 
-application = create_app()
 
 if __name__ == '__main__':
-    application.run(debug = True)
+    application.run()
